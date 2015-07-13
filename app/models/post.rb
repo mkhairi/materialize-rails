8e58d46b846efbcae27359f2b5bc815a46015179
Mohd Khairi Mohd Adnan <system@dev.localhost.my>
2015-07-13 10:53:47 +0800

    form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  validates :title, :body, presence: :true
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module MaterializeApps
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
