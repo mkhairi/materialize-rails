bd7df14a044865592b878ace9c511845a2618613
mkhairi <khairi@labs.my>
2015-09-17 19:10:19 +0800

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize-sprockets
+//= require materialize/extras/nouislider
 //= require init
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  attr_accessor :gender
   validates :title, :body, presence: :true
 end
