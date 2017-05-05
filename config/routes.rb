8f15bc7b50a4212d98ce18e47509b56e0e3d0ee6
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-05-05 17:47:03 +0800

    readd basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    resources :posts
+
     get  "avatar(/:size/:text)", to: "avatar#index",    as: :avatar
     get  "/*id",                 to: 'pages#show',      as: :page, format: false, constraints: HighVoltage::Constraints::RootRoute
     root 'pages#home'
