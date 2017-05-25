485f3b4e1bbf831560b0ce8abce91433f454df67
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-05-25 13:24:54 +0800

    update layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  layout :set_layout
 
   protect_from_forgery with: :exception
 
+  # optional just for fun
   helper_method :current_hit
 
   def current_hit
@@ -13,6 +15,17 @@ class ApplicationController < ActionController::Base
     hit += 1
     cookies['hit'] = hit
     hit
+  end
+
+  protected
+
+
+  def set_layout
+    if user_signed_in?
+      "application"
+    else
+      devise_controller? ? "login" : "landing"
+    end
   end
 
 end
