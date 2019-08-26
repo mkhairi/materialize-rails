9221a83bf7b0ad835738fc1ab75678572770b51a
Mohd Khairi Mohd Adnan <khairi@labs.my>
2019-08-26 16:43:13 +0800

    update migration

--- db/migrate/20170806125915_create_active_storage_tables.rb
+++ db/migrate/20170806125915_create_active_storage_tables.rb
@@ -1,3 +1,4 @@
+# This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
     create_table :active_storage_blobs do |t|
@@ -20,6 +21,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.datetime :created_at, null: false
 
       t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
+      t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
 end
